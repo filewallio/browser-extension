c7de18f152e5fdd0134f95e572b55605ade17431
Timothy Beene <tibeene@gmail.com>
2020-01-27 16:36:40 -0600

    move web-ext config to file

---
+++ web-ext-config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    // verbose: true,
+    run: {
+        startUrl: [
+            'https://duckduckgo.com/?q=sample+pdf',
+            'about:devtools-toolbox?type=extension&id=firefox-extension%40filewall.io',
+        ]
+    }
+};
