7a51f9ae59339baf31aa1891e574eec085c3110f
Timothy Beene <tibeene@gmail.com>
2020-02-20 14:35:09 -0600

    add start url to dev env

--- web-ext-config.js
+++ web-ext-config.js
@@ -6,6 +6,7 @@ module.exports = {
         keepProfileChanges: true,
         startUrl: [
             'https://duckduckgo.com/?q=sample+pdf',
+            'https://duckduckgo.com/?q=http%3A%2F%2Fi.isha.ws%2Fpublic%2Febooks%2Fmystics-musings-preview.pdf&ia=web',
             'about:devtools-toolbox?type=extension&id=firefox-extension%40filewall.io',
         ]
     }
