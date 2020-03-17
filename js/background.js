787f2f40f218876181118f84fcadb16ba3915cbc
Timothy Beene <tibeene@gmail.com>
2020-03-16 21:09:06 -0500

    take filename from onDeterminingFilename

--- js/background.js
+++ js/background.js
@@ -3,6 +3,8 @@ import { storage } from './storage.js';
 import { downloader } from './downloader.js';
 const browser = require('webextension-polyfill');
 
+let shouldCatchUrls = [];
+// TODO only call on change of value instead of each time store is updated
 storage.onChange().subscribe(store => {
 
     if (store['enable-context-menu'] ) {
@@ -24,7 +26,11 @@ storage.onChange().subscribe(store => {
 // listen for clicks to contextMenu
 browser.contextMenus.onClicked.addListener((info, tab) => {
     if (info.menuItemId === 'secure_download') {
-        downloader.addDownload(info.linkUrl);
+        const { linkUrl } = info;
+        // add url to shouldCatchUrls array
+        shouldCatchUrls.push(linkUrl);
+
+        browser.downloads.download({url: linkUrl});
     }
 });
 
@@ -35,59 +41,80 @@ browser.contextMenus.onClicked.addListener((info, tab) => {
 // This will catch normal downloads
 // CREATE
 browser.downloads.onCreated.addListener( downloadItem => {
-    console.log('downloads.onCreated', downloadItem)
-
-    storage.appDataAsync().then(store => {
-        if (store['catch-all-downloads']  === true) {
-
-            if (downloader.wasConfirmedDirect(downloadItem.url)){
-                return;
-            }
-
-            var baseurls = [
-                "https://filewall.io"   ,
-                "https://eu.filewall.io",
-                "https://us.filewall.io",
-                "http://127.0.0.1",
-                "chrome-extension://"
-            ];
-            const { origin } = new URL(downloadItem.url)
-            for (const baseUrl of baseurls) {
-                    if ( origin === baseUrl ) {
-                        return;
-                }
-            }
-            // cancel existing download
-            browser.downloads.cancel(downloadItem.id);
-            if (downloadItem.state === "complete") {
-                browser.downloads.removeFile(downloadItem.id);
-            }
-            browser.downloads.erase({id: downloadItem.id});
-
-            // ask user
-            downloader.addCatchedDownload(downloadItem.url);
-        }
-    });
+    console.log('downloads.onCreated', downloadItem.filename, downloadItem)
 });
 
 // Get confirm messages from dialog iframe     // TODO xss?
 browser.runtime.onMessage.addListener((request) => {
-    if (request.download_id){
-        downloader.confirmCatchedDownload(request.download_id, request.action)
+    if (request.download_id) {
+        downloader.confirmCatchedDownload(request.download_id, request.action);
     }
 });
 
 // DETERMINE FILENAME
-// browser.downloads.onDeterminingFilename.addListener(function (item, suggest) {
-//     console.log('onDeterminingFilename', item);
-//     storage.appDataAsync().then(appData => {
-//         if (appData['catch-all-downloads']  === true) {
-//             downloader.onDeterminingFilename(item.url, item.filename)
-//         }
-//     })
-//     return true;
-// });
+browser.downloads.onDeterminingFilename.addListener(function (downloadItem, suggest) {
+    console.log('onDeterminingFilename', downloadItem.filename, downloadItem);
+
+    // is the url in shouldCatchUrls
+    const shouldCatchDownload = !!shouldCatchUrls.find(url => url === downloadItem.url);
+    if (shouldCatchDownload) {
+        
+        // remove downloadUrl for shouldCatchUrls
+        shouldCatchUrls = shouldCatchUrls.filter(url => url !== downloadItem.url);
+        
+        // cancel download and init filewall upload
+        removeDownload(downloadItem);
+        downloader.addDownload(downloadItem.url, downloadItem.filename);
+    } else {
+        storage.appDataAsync().then(store => {
+            if (store['catch-all-downloads']  === true) {
+    
+                if (downloader.wasConfirmedDirect(downloadItem.url)){
+                    return;
+                }
+    
+                var baseurls = [
+                    "https://filewall.io"   ,
+                    "https://eu.filewall.io",
+                    "https://us.filewall.io",
+                    "http://127.0.0.1",
+                    "chrome-extension://"
+                ];
+                const { origin } = new URL(downloadItem.url)
+                for (const baseUrl of baseurls) {
+                        if ( origin === baseUrl ) {
+                            return;
+                    }
+                }
+                // cancel existing download
+                browser.downloads.cancel(downloadItem.id);
+                if (downloadItem.state === "complete") {
+                    browser.downloads.removeFile(downloadItem.id);
+                }
+                browser.downloads.erase({id: downloadItem.id});
+    
+                // ask user
+                downloader.addCatchedDownload(downloadItem.url);
+            }
+        });
+    }
+    suggest();
+});
 // CATCH COMPLETED DOWNLOAD
-browser.downloads.onChanged.addListener(function (downloadDelta) { });
+// browser.downloads.onChanged.addListener(function (downloadDelta) { });
 // CATCH ERASE
 // browser.downloads.onErased.addListener(function (downloadId) { });
+
+async function removeDownload(downloadItem) {
+    const { state, id } = downloadItem;
+    // pause existing download
+    console.log(id, 'trying to pause state: ' + state)
+    await browser.downloads.pause(id);
+    // if (state === 'complete') {
+    //     console.log(id, 'trying to removeFile state: ' + state)
+    //     await browser.downloads.removeFile(id);
+    // }
+    // erase download
+    console.log(id, 'trying to erase state: ' + state)
+    await browser.downloads.erase({id});
+}
