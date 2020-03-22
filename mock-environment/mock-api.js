097446f24348dff184b118d0c47e341615f4f0d8
Timothy Beene <tibeene@gmail.com>
2020-03-22 18:17:01 -0500

    Change " to '

--- mock-environment/mock-api.js
+++ mock-environment/mock-api.js
@@ -4,7 +4,7 @@ var req = interfake.post('/account/api/')
     .status(200).body({ apikey: '1234-1234-1234-1234' });
     // .status(401).body({ })
 interfake.post('/api/authorize')
-    .status(202).body({"links": {"self":"http://localhost:3000/api/get/abbc7c94-h11y-4r27-92fg-czd3362f464a","upload": "http://localhost:3000/api/upload/abcd_very_long_example"}})
+    .status(202).body({'links': {'self':'http://localhost:3000/api/get/abbc7c94-h11y-4r27-92fg-czd3362f464a','upload': 'http://localhost:3000/api/upload/abcd_very_long_example'}})
     // .status(400).body({'error': 'invalid_request'});
     // .status(402).body({'error': 'payment_required'});
     // .status(403).body({'error': 'auth_failed'});
@@ -12,7 +12,7 @@ interfake.post('/api/authorize')
     // .status(429).body({'error': 'too_many_requests'});
 interfake.post('/api/upload/abcd_very_long_example')
     .status(202).body({});
-    // .status(400).body({ "error": "invalid_request" });
+    // .status(400).body({ 'error': 'invalid_request' });
 interfake.get('/api/get/abbc7c94-h11y-4r27-92fg-czd3362f464a')
     // .status(200).body({ 'status': 'waiting' })
     .status(200).body({ 'status': 'processing' })
