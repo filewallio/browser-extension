a33b7d794deaadf721386a75388d52d2e1e1755a
cyberdock <office@cyberdock.de>
2020-04-23 02:31:01 +0200

    some cleanups

--- js/dialog/dialog.js
+++ js/dialog/dialog.js
@@ -1,16 +1,13 @@
-import { storage } from '../storage'
-
 const $ = document.querySelector.bind(document);
 const $s = document.querySelectorAll.bind(document);
 const browser = require('webextension-polyfill');
 
 window.addEventListener('load', function () {
-    console.log('in dialog/dialog.js')
 
     const download_id = new URLSearchParams(window.location.search).get('download_id');
     const filename = new URLSearchParams(window.location.search).get('filename');
 
-    $('#fwiodli_filename').innerHTML  = filename;
+    $('#fwiodli_filename').textContent  = filename;
 
     $('#fwiodli_direct').addEventListener('click', e => {
         e.preventDefault();
